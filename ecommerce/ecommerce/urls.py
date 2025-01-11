801f4eab498473f69ea8bcd3903154bf6498edfd
reDevState <165166059+reDevState@users.noreply.github.com>
2025-01-11 01:45:33 +0000

    Update main Front end update

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
+    
+    path('', include('core.urls', namespace='core')), 
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
